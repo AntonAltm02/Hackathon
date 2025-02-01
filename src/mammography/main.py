27754e14d6281c88c48ec9ee9526cedda9171b31
Anton Altmeyer <anton.altm@outlook.de>
2025-02-01 10:19:12 +0100

    commented the instance of DCUNet and included relative path

--- src/mammography/main.py
+++ src/mammography/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import evaluation
 from process import get_images
-from DCUNet import DCUNet
+# from DCUNet import DCUNet
 import efficientNet
 from evaluation import tversky, tversky_loss, focal_tversky, dice_coef, dice_coef_loss, jacard
 import numpy as np
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     script_dir = os.path.dirname(__file__)
     data_path = os.path.join(script_dir + "/../data")
 
-    dicom_info = pd.read_csv(data_path + "/dicom_info.csv")
-    image_dir = data_path + "/jpeg"
+    dicom_info = pd.read_csv(data_path + "/CBIS-DDSM/dicom_info.csv")
+    image_dir = data_path + "/CBIS-DDSM/jpeg"
 
     print(dicom_info.head())
     print(dicom_info.info())
@@ -29,9 +29,9 @@ if __name__ == '__main__':
     Y = np.array((2000, 249, 249))
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=5)
 
-    dcunet = DCUNet(height=250, width=250, channels=1)
-    dcunet.compile(optimizer='adam', loss=focal_tversky, metrics=[dice_coef, jacard, 'accuracy'])
-    dcunet.summary()
+    # dcunet = DCUNet(height=250, width=250, channels=1)
+    # dcunet.compile(optimizer='adam', loss=focal_tversky, metrics=[dice_coef, jacard, 'accuracy'])
+    # dcunet.summary()
 
     effnetV2 = efficientNet.effnetv2_s()
     effnetV2.summary()
